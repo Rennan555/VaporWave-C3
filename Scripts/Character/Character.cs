f78b785521e07070dc6a90e98d3baead52657acf
Rennan555 <dungeonbr5@gmail.com>
2026-07-24 11:27:10 -0300

    feat: Initial Character and Player

---
+++ Scripts/Character/Character.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public abstract partial class Character : CharacterBody2D
+{
+	[Export] public float Speed = 300.0f;
+	[Export] public float JumpVelocity = -400.0f;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+		
+		MoveAndSlide();
+	}
+}

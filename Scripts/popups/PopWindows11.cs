af2f11bb93408a3a1718d12d8248dfcdbd7eddba
GarotoLinux <garotolinux@gmail.com>
2026-07-24 21:20:40 -0300

    pop-windowsV1

---
+++ Scripts/popups/PopWindows11.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class PopWindows11 : Control
+{
+	/*
+	//sinal 
+	[Signal]
+	public delegate void BtnceitarEventHandler();
+	[Signal]
+	public delegate void BtnRecusarEventHandler();*/
+
+	[Export] public bool condicao;
+	private bool choice;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (choice) {
+			GD.Print(condicao);
+		}
+	}
+
+	public void _on_btn_recusar_pressed()
+	{
+		if (!choice) {
+		condicao = true;
+		choice = true;
+		EmitSignal(SignalName.Resposta, condicao);
+		}
+	}
+
+	public void _on_btn_aceitar_pressed()
+	{	
+		if (!choice) {
+		condicao = false;
+		choice = true;}
+	}
+
+
+	[Signal]
+	public delegate void RespostaEventHandler(bool response);
+}

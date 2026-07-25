23598299c9399822eff0b62f35b336783206f034
GarotoLinux <garotolinux@gmail.com>
2026-07-25 11:04:32 -0300

    popUps

--- Scripts/popups/PopUpNotas.cs
+++ Scripts/popups/PopUpNotas.cs
@@ -1,5 +1,8 @@
 using Godot;
 using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 /*Algoritmo para gerenciar o bloco de notas:
 os textos,
@@ -10,38 +13,92 @@ public partial class PopUpNotas : Area2D
 	private RichTextLabel text;
 	private Control popUp;
 
+	private Timer time;
+
+	private AnimationPlayer animationText;
+
+	private Label textInteraact;
 	//variaveis 
 	private bool canClose; // será usado para determinar quando fechar a janela e desativar o bloco de notas
 	private int linhaTexto;
+
+	private bool canInteract = true;
+
+	private PopWindows11 windows11;
+
+	[Export] public bool show;
+
+	[Export] public string notes = "lorem jad jdad jad jad";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//chamado as variaveis
-		text = GetNode<RichTextLabel>("Control/RichTextLabel");
+		text = GetNode<RichTextLabel>("popUp/Panel/RichTextLabel");
 		popUp = GetNode<Control>("popUp");
+		time = GetNode<Timer>("Timer");
+		animationText = GetNode<AnimationPlayer>("popUp/Panel/RichTextLabel/AnimationText");
+		textInteraact = GetNode<Label>("ActionArea/Label");
+		windows11 = GetNode<PopWindows11>("Pop_windows11");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		//se a variavel show for verdadeira
+		if (show && !canClose)
+		{
+			ShowPopUp();
+			animationText.Play("write");
+			
+			if (text.VisibleRatio >= 0.09f)
+			{
+				GD.Print("");
+				animationText.Play("full");
+				canClose = true;
+			}
+		}
+
 	}
 
-	public void _on_close_pressed()
+	//fechar a janela
+	public void  _on_close_pressed()
 	{
+		GD.Print("fechou");
 		//QueueFree();
 		if (canClose) {
 		popUp.Visible = false;
+		windows11.Visible = true;
+
+		//adicionar a tela de atualizacao
+
 		}
 	}
 
-	public void showPopUp()
+	//mostrar a janela do bloco de notas
+	public void ShowPopUp()
 	{
 		popUp.Visible = true;
 	}
 
-	public void editText()
+	public void _on_action_area_body_entered(Node2D body)
 	{
+		if (canInteract)
+		{
+			textInteraact.Visible = true;
+		}
 		
+
+		//implementar a funcao para interagir
+		if (Input.IsActionJustPressed("acao")){
+			canInteract = false;
+			show = true;
+		}
 	}
+
+	public void _on_action_area_body_exited(Node2D body)
+	{
+		textInteraact.Visible = false;
+	}
+
+	
 }

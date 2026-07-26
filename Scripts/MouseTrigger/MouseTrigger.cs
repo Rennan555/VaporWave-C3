54d9fd701154dad8721e47be054f4c6cab092e9c
Rennan555 <dungeonbr5@gmail.com>
2026-07-26 02:26:53 -0300

    Initial Mouse Trigger added

---
+++ Scripts/MouseTrigger/MouseTrigger.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class MouseTrigger : Area2D
+{
+	public override void _Ready()
+	{
+	}
+	
+	public override void _Process(double delta)
+	{
+		Position = GetGlobalMousePosition();
+	}
+	
+	public void BodyEntered(Node2D body)
+	{
+		if (body is Enemy enemy)
+		{
+			GD.Print(enemy.Name);
+		}
+	}
+}
